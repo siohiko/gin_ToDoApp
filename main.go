604a25844691ea92c068fe54690b78643c16d88a
siokawa yuusuke <siokawa0476@gmail.com>
2020-10-08 15:55:08 +0900

    make login(top)page and registar page

--- main.go
+++ main.go
@@ -7,14 +7,24 @@ import (
 
 func main() {
 	router := gin.Default()
-
 	router.Static("styles", "./styles")
 	router.LoadHTMLGlob("templates/*")
 
-	router.GET("/index", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"title": "Top Page",
-		})
-	})
+	//	v1 route
+	v1 := router.Group("/v1")
+	{
+		v1.GET("/top", topPageEndPoint)
+		v1.GET("/create_account_page", createAccountPageEndPoint)
+	}
 	router.Run(":8080")
+}
+
+func topPageEndPoint(c *gin.Context) {
+	c.HTML(http.StatusOK, "top.tmpl", gin.H{
+		"title": "Top Page",
+	})
+}
+
+func createAccountPageEndPoint(c *gin.Context) {
+	c.HTML(http.StatusOK, "createAccount.tmpl", gin.H{})
 }
